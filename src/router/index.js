f2830df7901abcb2ffa7d9f8fcb18434e6c733ea
honnefsena <neiajonsena@gmail.com>
2025-01-25 10:05:49 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,47 @@
-// src/router/index.js
 import { createRouter, createWebHistory } from "vue-router"
 
 const routes = [
-  {
-    path: "/",
-    redirect: "/products",
-  },
-  {
-    path: "/products",
-    name: "Products",
-    component: () => import("@/views/Products.vue"),
-  },
-  {
-    path: "/inventory",
-    name: "Inventory",
-    component: () => import("@/views/Inventory.vue"),
-  },
-  {
-    path: "/sales",
-    name: "Sales",
-    component: () => import("@/views/Sales.vue"),
-  },
-  {
-    path: "/reports",
-    name: "Reports",
-    component: () => import("@/views/Reports.vue"),
-  },
+ {
+   path: "/",
+   redirect: "/products",
+ },
+ {
+   path: "/products",
+   name: "Products", 
+   component: () => import("@/views/Products.vue"),
+ },
+ {
+   path: "/inventory",
+   name: "Inventory",
+   component: () => import("@/views/Inventory.vue"),
+ },
+ {
+   path: "/sales",
+   name: "Sales",
+   component: () => import("@/views/Sales.vue"),
+ },
+ {
+   path: "/reports",
+   name: "Reports",
+   component: () => import("@/views/Reports.vue"),
+ },
+ {
+   path: "/:pathMatch(.*)*",
+   redirect: "/"
+ }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+ history: createWebHistory(),
+ routes,
+})
+
+router.beforeEach((to, from, next) => {
+ if (!to.matched.length) {
+   next({ path: '/' })
+   return
+ }
+ next()
 })
 
 export default router
