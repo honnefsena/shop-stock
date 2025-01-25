7e5abb26038ef62e1f1ed3cccb89f7ed47d1ccb1
honnefsena <neiajonsena@gmail.com>
2025-01-25 10:01:25 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-// vite.config.js
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import path from "path"
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://setelip.com.br/shop-stock-jumap/",
-        changeOrigin: true,
-      },
-    },
-  },
+ plugins: [vue()],
+ resolve: {
+   alias: {
+     "@": path.resolve(__dirname, "./src"),
+   },
+ },
+ server: {
+   proxy: {
+     "/api": {
+       target: "https://setelip.com.br/shop-stock-jumap/",
+       changeOrigin: true,
+     },
+   },
+   historyApiFallback: true
+ }
 })
